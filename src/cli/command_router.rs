9cd8d05c7a672d1edfe6f0a59b4d5a9f11c2bfac
bituan <113036994+bituann@users.noreply.github.com>
2025-07-07 19:07:25 +0100

    Update list command invocation

--- src/cli/command_router.rs
+++ src/cli/command_router.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 pub fn route_command (command: &str, arg: &str) -> enums::Outcome<String> {
 	match command {
 		"upload" => upload::Upload::new(arg).execute(),
-		"list" => list::List::new().execute(),
+		"list" => list::List::new(arg).execute(),
 		"read" => read::Read::new(arg).execute(),
 		"delete" => delete::Delete::new(arg).execute(),
 		_ => enums::Outcome::Fail(String::from("Invalid command")),
