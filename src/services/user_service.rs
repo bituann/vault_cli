7a5551b3f0a65ff9db9e263c8540fc9472df2a94
bituan <113036994+bituann@users.noreply.github.com>
2025-07-08 16:26:58 +0100

    Add user structure and hashing function

--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,12 +1,36 @@
 use std::io;
+use std::hash::{DefaultHasher, Hash, Hasher};
 use crate::utils::enums;
+use serde::{Serialize, Deserialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+	id: String,
+	email: String,
+	password: String,
+}
 
 pub fn register (email: &String, password: &String) -> enums::Outcome<String> {
 	
+	//
 	enums::Outcome::Success(String::from(""))
 }
 
 pub fn login (email: &String, password: &String) -> enums::Outcome<String> {
 	
 	enums::Outcome::Success(String::from(""))
+}
+
+fn calculate_hash<T: Hash> (value: &T) -> String {
+	//create hasher
+	let mut hasher = DefaultHasher::new();
+	
+	//hash the value
+    value.hash(&mut hasher);
+    
+    //get hashed value
+    let hash = hasher.finish();
+    
+    return hash.to_string();
 }
