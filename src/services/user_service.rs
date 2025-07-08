d6b286715d615d5131002db907e4747b2e7c659b
bituan <113036994+bituann@users.noreply.github.com>
2025-07-08 17:41:23 +0100

    Write function to register user

--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,8 +1,10 @@
-use std::io;
+use std::io::{Read, Write};
 use std::hash::{DefaultHasher, Hash, Hasher};
+use std::fs;
 use crate::utils::enums;
 use serde::{Serialize, Deserialize};
 use serde_json::Result;
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
 struct User {
@@ -11,10 +13,27 @@ struct User {
 	password: String,
 }
 
-pub fn register (email: &String, password: &String) -> enums::Outcome<String> {
+pub fn register (email: String, password: String) -> enums::Outcome<String> {
+	//generate user id
+	let id = Uuid::new_v4().to_string();
 	
-	//
-	enums::Outcome::Success(String::from(""))
+	//hash password
+	let password = calculate_hash(&password);
+	
+	//create user struct
+	let user = User { id, email, password };
+	
+	//add user to database
+	match add_user(user) {
+		enums::Outcome::Success(()) => {
+			let msg = String::from("User successfully registered");
+			return enums::Outcome::Success(msg);
+		},
+		enums::Outcome::Fail(msg) => {
+			let msg = String::from("Unable to register user");
+			return enums::Outcome::Fail(msg);
+		}
+	}
 }
 
 pub fn login (email: &String, password: &String) -> enums::Outcome<String> {
@@ -22,6 +41,16 @@ pub fn login (email: &String, password: &String) -> enums::Outcome<String> {
 	enums::Outcome::Success(String::from(""))
 }
 
+
+
+
+
+
+
+
+
+
+//private helper functions
 fn calculate_hash<T: Hash> (value: &T) -> String {
 	//create hasher
 	let mut hasher = DefaultHasher::new();
@@ -33,4 +62,76 @@ fn calculate_hash<T: Hash> (value: &T) -> String {
     let hash = hasher.finish();
     
     return hash.to_string();
+}
+
+fn get_users_as_vec () -> enums::Outcome<Vec<User>> {
+	let mut users_data;
+	let path = "./src/storage/userdata.json";
+	
+	//open json file
+	match fs::File::open(path) {
+		Ok(file) => users_data = file,
+		Err(_) => {
+			let msg = String::from("Unable to access database");
+			return enums::Outcome::Fail(msg);
+		}
+	}
+	
+	//deserialize json file into vec
+	match serde_json::from_reader(&users_data) {
+		Ok(users) => enums::Outcome::Success(users),
+		Err(_) => enums::Outcome::Success(Vec::new()),
+	}
+}
+
+fn add_user (user: User) -> enums::Outcome<()> {
+	let mut users: Vec<User>;
+	
+	match get_users_as_vec() {
+		enums::Outcome::Success(a) => users = a,
+		enums::Outcome::Fail(msg) => return enums::Outcome::Fail(msg),
+	}
+	
+	users.push(user);
+	return update_userdata_json(users);
+}
+
+
+
+fn update_userdata_json (users: Vec<User>) -> enums::Outcome<()> {
+	let path = "./src/storage/userdata.json";
+	
+	//convert vec to peekable. This allows us to look ahead to the next value before getting to it
+	let mut users = users.iter().peekable();
+	let mut userdata;
+	
+	//open json file to be overwritten
+	match fs::File::create(path) {
+		Ok(file) => userdata = file,
+		Err(_) => {
+			let msg = String::from("Cannot access database");
+			return enums::Outcome::Fail(msg);
+		}
+	}
+	
+	//begin creating json string
+	let mut json_string = "[".to_string();
+	
+	//add each file as a json entry
+	while let Some(user) = users.next() {
+		if users.peek().is_none() {
+			json_string = format!("{}{}", json_string, serde_json::to_string(&user).unwrap());
+			break;
+		}
+		
+		json_string = format!("{}{},", json_string, serde_json::to_string(&user).unwrap());
+	}
+	
+	//conclude json string
+	json_string = format!("{}{}", json_string, "]");
+	
+	//write json string to json file
+	userdata.write(json_string.as_bytes());
+	
+	return enums::Outcome::Success(());
 }
